f88f886eae0d53b66f0b77c1d62f3038aea67cd1
Navod Linux <navod199736@gmail.com>
2019-11-25 13:08:02 +0530

    done !

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-})
+  template:"<p>loading ... </p>"
+ })
 export class HomePage {
 
-  constructor() {}
+  constructor(private iab: InAppBrowser,public platform: Platform) { 
+
+    let webSite = "https://www.google.com";
+
+    platform.ready().then(() => {
+
+      // const browser = this.iab.create('https://ionicframework.com/');
+
+      // browser.close();
+
+      let browser = this.iab.create( webSite,'_blank',{zoom:'no',location:'no'});
+     
+      browser.show();
+     
+      
+    });
+
+  }
 
 }
